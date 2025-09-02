d0d96107cadca667e7a16c73c4ee4b543ea066ee
souravdas090300 <souravdas090300@gmail.com>
2025-09-02 17:55:53 +0200

    Fix API URL and configure GitHub Pages deployment

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -5,7 +5,7 @@ import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 //Declaring the api url that will provide data for the client app
-const apiUrl = 'https://sourav-movie-api-b47b9d7a9ae5.herokuapp.com/';
+const apiUrl = 'https://my-flix-db.herokuapp.com/';
 @Injectable({
   providedIn: 'root'
 })
