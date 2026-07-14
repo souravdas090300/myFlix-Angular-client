3e5c5f468ee407b02469608ab22c3c65b6d7f6f0
souravdas090300 <souravdas090300@gmail.com>
2026-07-14 19:49:01 +0200

    Updated enviroment.prod
    Updated render url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   production: true,
   // Use the deployed Heroku API directly in production
   // This is what the tutor expects for the delivered application
-  apiUrl: 'https://ancient-woodland-05995-715624a89d87.herokuapp.com/'
+  apiUrl: 'https://movieflix-api-p56c.onrender.com/'
 };
